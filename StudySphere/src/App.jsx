50ad43de1ee78f126ffa807c157852424cc0000f
Tanvi Kadam <tanvie029@gmail.com>
2026-07-17 02:01:09 +0530

    create initial navbar layout

--- StudySphere/src/App.jsx
+++ StudySphere/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
   return (
-    <h1>Hello Tanvi!</h1>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ StudySphere/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <nav>
+            <h1>StudySphere</h1>
+
+            <ul>
+                <li>Dashboard</li>
+                <li>Subjects</li>
+                <li>Tasks</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
